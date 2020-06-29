d8a5a4aa1e95672352ceae20544beb4e359b8d31
RandiSPB <60474777+RandiSPB@users.noreply.github.com>
2020-06-29 14:42:36 +0300

    Update LR1.py

--- LR1.py
+++ LR1.py
@@ -1,43 +1,192 @@
-import pandas
-from pandas import read_csv
-import re
-import string
-parced_csv = pandas.read_csv('train.csv',sep=',', index_col='PassengerId', quotechar='"')
+# Программирование (Python)
+# 6 семестр, тема 1
 
-train = read_csv("train.csv")
-sex = train[train.Sex == u'male']
-print('Task_1:', len(sex), 'male', 'and', len(train) - len(sex), 'female')
-print('Task_1:', (len(sex)*100)/len(train), '%male', 'and', 100 - (len(sex)*100)/len(train), '%female')
-port_s = train[train.Embarked == u'S']
-port_c = train[train.Embarked == u'C']
-port_q = train[train.Embarked == u'Q']
-print('Task_2:', 'port S -', len(port_s), 'port C -', len(port_c), 'port Q -', len(port_q))
-died = train[train.Survived == 0]
-print('Task_3:', 'Died', len(died), '%Died', len(died)/len(train)*100)
-class_t = train[train.Pclass ==  3]
-class_s = train[train.Pclass ==  2]
-class_f = train[train.Pclass ==  1]
-print('Task_4:', 'First class -', len(class_f)/len(train)*100,'Second class -', len(class_s)/len(train)*100,'Third class -', len(class_t)/len(train)*100)
-cor1 = train["SibSp"].corr(train["Parch"], method = 'pearson')
-print('Task_5:', 'find corr sibsp parch =', cor1)
-cor2 = train["Age"].corr(train["Survived"], method = 'pearson')
-print('Task_6.1:', 'find corr age survived =', cor2)
-#cor3 = train["Sex"].corr(train["Survived"], method = 'pearson')
-print('Task_6.2:', 'find corr sex survived = TypeError: unsupported operand type(s) for /: str and int')
-cor4 = train["Pclass"].corr(train["Survived"], method = 'pearson')
-print('Task_6.3:', 'find corr Pclass survived =', cor4)
-print('Task_7:', 'Avg =', train["Age"].mean(), 'median =', train["Age"].median())
-print('Task_8:', 'Avg =', train["Fare"].mean(), 'median =', train["Fare"].median())
-name = train
-name.reset_index(inplace = True, drop = True)
-for i in range(len(name)):
-    str = name["Name"][i]
-    res = re.sub('['+string.punctuation+']', '', str).split()
-    name.loc[i, 'Name'] =  res[2]
-nameMan = name.loc[name["Sex"] == 'male']
-print('Task_9: Popular man name', nameMan["Name"].value_counts().index[0])
-nameMan = nameMan.loc[nameMan["Age"] > 15]
-print('Task_10: Popular man name, when age > 15', nameMan["Name"].value_counts().index[0])
-nameWoman = name.loc[name["Sex"]  == 'female']
-nameWoman = nameWoman.loc[nameWoman["Age"] > 15]
-print('Task_10: Popular woman name, when age > 15', nameWoman["Name"].value_counts())
+# Лабораторная работа 1
+
+"""
+Используя обучающий набор данных о пассажирах Титаника, находящийся в проекте (оригинал: https://www.kaggle.com/c/titanic/data), найдите ответы на следующие вопросы: 
+
+3. Посчитайте долю погибших на параходе (число и процент)?
+
+4. Какие доли составляли пассажиры первого, второго, третьего класса?
+
+5. Вычислите коэффициент корреляции Пирсона между количеством супругов (SibSp) и количеством детей (Parch).
+
+6. Выясните есть ли корреляция (вычислите коэффициент корреляции Пирсона) между:
+1) возрастом и параметром survival;
+2) полом человека и параметром survival;
+3) классом, в котором пассажир ехал, и параметром survival.
+
+7. Посчитайте средний возраст пассажиров и медиану.
+8. Посчитайте среднюю цену за билет и медиану.
+
+9. Какое самое популярное мужское имя на корабле?
+10. Какие самые популярные мужское и женские имена людей, старше 15 лет на корабле?
+
+"""
+import pandas
+
+parsed_csv = pandas.read_csv('train.csv', sep=',', index_col='PassengerId', quotechar='"')
+
+
+def get_sex_distrib(data):
+    """
+    1. Какое количество мужчин и женщин ехало на пароходе? Приведите два числа через пробел.
+    """
+    return data['Sex'].value_counts()
+
+
+def get_port_distrib(data):
+    """  
+    2. Подсчитайте, сколько пассажиров загрузилось на борт в различных портах? Приведите три числа через пробел.
+    """
+
+    port = data['Embarked'].value_counts()
+    return port['S'], port['Q'], port['C']
+
+
+def get_surv_percent(data):
+    """
+    3. Посчитайте долю погибших на пароходе (число и процент).
+    """
+    died, survived = data['Survived'].value_counts()
+    survival_ratio = (survived / (survived + died)) * 100
+    dying_ratio = (died / (survived + died)) * 100
+    return survived, survival_ratio, died, dying_ratio
+
+
+def get_class_distrib(data):
+    """
+    4. Какие доли составляли пассажиры первого, второго, третьего класса?    
+    """
+    ticket_class = data['Pclass'].value_counts()
+    first_class, second_class, third_class = ticket_class
+    first_class_ratio = (first_class / sum(ticket_class)) * 100
+    second_class_ratio = (second_class / sum(ticket_class)) * 100
+    third_class_ratio = (third_class / sum(ticket_class)) * 100
+    class_stats = (
+            first_class, first_class_ratio,
+            second_class, second_class_ratio,
+            third_class, third_class_ratio
+            )
+    return class_stats
+
+
+def find_corr_sibsp_parch(data):
+    """
+    5. Вычислите коэффициент корреляции Пирсона между количеством супругов (SibSp) и количеством детей (Parch).
+    """
+    return data['SibSp'].corr(data['Parch'], method='pearson')
+
+
+def find_corr_age_survival(data):
+    """
+    6.1. Выясните, есть ли корреляция (вычислите коэффициент корреляции Пирсона) между:
+    - возрастом и параметром survival;
+    """
+    return data['Survived'].corr(data['Age'], method='pearson')
+
+
+def find_corr_sex_survival(data):
+    """
+    6.2. Выясните, есть ли корреляция (вычислите коэффициент корреляции Пирсона) между:
+    - полом человека и параметром survival;
+    """
+    gender_df = pandas.Series([1 if i == 'male' else 0 for i in data['Sex']])
+    return data['Survived'].corr(gender_df, method='pearson')
+
+
+def find_corr_class_survival(data):
+    """
+    6.3. Выясните, есть ли корреляция (вычислите коэффициент корреляции Пирсона) между:
+    - классом, в котором пассажир ехал, и параметром survival.
+    """
+    return data['Survived'].corr(data['Pclass'], method='pearson')
+
+
+def find_pass_mean_median(data):
+    """
+    7. Посчитайте средний возраст пассажиров и медиану.
+    """
+    avg_age = parsed_csv['Age'].mean(skipna=True)
+    median_age = parsed_csv['Age'].median(skipna=True)
+    return avg_age, median_age
+
+
+def find_ticket_mean_median(data):
+    """
+    8. Посчитайте среднюю цену за билет и медиану.
+    """
+    avg_fare = parsed_csv['Fare'].mean(skipna=True)
+    median_fare = parsed_csv['Fare'].median(skipna=True)
+    return avg_fare, median_fare
+
+
+def find_popular_name(data):
+    """
+    9. Какое самое популярное мужское имя на корабле?
+    """
+    names = [i.split(', ')[1] for i in parsed_csv['Name']]
+    sorted_names = pandas.Series(names).value_counts()
+    return sorted_names.index[0], sorted_names.iat[0]
+
+
+def find_popular_adult_names(data):
+    """
+    10. Какие самые популярные мужское и женские имена людей, старше 15 лет на корабле?
+    """
+    older_male = parsed_csv[(parsed_csv['Age'] > 15) & (parsed_csv['Sex'] == 'male')]
+    older_female = parsed_csv[(parsed_csv['Age'] > 15) & (parsed_csv['Sex'] == 'female')]
+
+    male_names = [i.split(', ')[1] for i in older_male['Name']]
+    male_sorted_names = pandas.Series(male_names).value_counts()
+
+    female_names = [i.split(', ')[1] for i in older_female['Name']]
+    female_sorted_names = pandas.Series(female_names).value_counts()
+    return male_sorted_names.index[0], male_sorted_names.iat[0], female_sorted_names.index[0], female_sorted_names.iat[0]
+
+
+def test_get_number_of_pass():
+    assert get_sex_distrib(parsed_csv) == (577, 314), "Количество мужчин и женщин на Титанике"
+
+
+def test_get_number_embarked():
+    assert get_sex_distrib(parsed_csv) == (644, 77, 168), "Количество пассажиров в разных портах на Титанике"
+
+# аналогично протестировать остальные функции
+
+
+print('-----\n1. gender\nmale: {}\nfemale: {}'.format(*get_sex_distrib(parsed_csv)))
+
+print('-----\n2. embarked')
+print('southampton: {}; queenstown: {}; cherbourg: {}'.format(*get_port_distrib(parsed_csv)))
+
+print('-----\n3. survival percent')
+print('survived: {} ({:.2f}%)\ndied: {} ({:.2f}%)'.format(*get_surv_percent(parsed_csv)))
+
+print('-----\n4. class stats')
+print('1: {} ({:.2f}%)\n2: {} ({:.2f}%)\n3: {} ({:.2f}%)'.format(*get_class_distrib(parsed_csv)))
+
+print('-----\n5. siblings-kids correlation')
+print('correlation: {}'.format(find_corr_sibsp_parch(parsed_csv)))
+
+print('-----\n6.1. age-survival correlation')
+print('correlation: {}'.format(find_corr_age_survival(parsed_csv)))
+
+print('-----\n6.2. gender-survival correlation')
+print('correlation: {}'.format(find_corr_sex_survival(parsed_csv)))
+
+print('-----\n6.3. class-survival correlation')
+print('correlation: {}'.format(find_corr_class_survival(parsed_csv)))
+
+print('-----\n7. avg & median age')
+print('average age: {}\nmedian age: {}'.format(*find_pass_mean_median(parsed_csv)))
+
+print('-----\n8. avg & median ticket fare')
+print('average fare: {}\nmedian fare: {}'.format(*find_ticket_mean_median(parsed_csv)))
+
+print('-----\n9. the most popular male name')
+print('the most popular male name is {} ({})'.format(*find_popular_name(parsed_csv)))
+
+print('-----\n10. the most popular >15yo name')
+print('male: {} ({})\nfemale: {} ({})'.format(*find_popular_adult_names(parsed_csv)))
