9c1a61420b862590465a7c63b44d806b747b6833
RandiSPB <60474777+RandiSPB@users.noreply.github.com>
2020-06-29 14:48:51 +0300

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -1,39 +1,57 @@
-"""
+import hashlib as hasher
+import datetime as date
+import string
+import itertools
 
-На основе кода, размещенного в файле blockchain.py, реализовать корутину (генератор), позволяющий с помощью метода send(), возвращать новый блок. Кроме механизма возврата нового блока требуется создать механизм, позволяющий вернуть историю сгенерированных блоков. 
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
 
-"""
+        self.hash = self.hash_block()
 
-import random
-from hashlib import sha3_512
-import binascii
+    def hash_block(self):
+        sha = hasher.sha256()
+        sha.update(
+            (str(self.index)+
+            str(self.timestamp)+
+            str(self.data)+
+            str(self.previous_hash)).encode('utf-8')
+        )
+        return sha.hexdigest()
 
-genesis_block = bytearray([0 for i in range(0, 64)])
-baseblock = genesis_block
+def create_genesis_block():
+    return Block(0,date.datetime.now, 'this is first block data', '0')
+
+def next_block(last_block):
+    current_timestamp = date.datetime.now()
+    current_index = last_block.index+1
+    current_data = str(current_index)+string.ascii_lowercase
+
+    current_hash = last_block.hash
+
+    return Block(current_index, current_timestamp, current_data, current_hash)
+
+number_of_blocks = 19
 
 
-def randomblock(limit):
-    for i in range(0, limit):
-        ml = sha3_512()
-        ml.update(str(random.randint(0, 1e18)).encode("iso-8859-1"))
-        yield bytearray(ml.hexdigest().encode("iso-8859-1"))
 
+def create_blockchain():
+    blockchain = [create_genesis_block()]
+    for index in itertools.count():
+        data = yield
+        block = next_block(blockchain[index])
+        yield block.hash
+        blockchain.append(block)
 
-def signedblock(baseblock, limit):
-    for block in randomblock(limit):
-        block_data = baseblock + block
-        digest = sha3_512()
-        digest.update(block_data)
-        baseblock = digest.digest()
-        yield block_data + digest.digest()
-
-
-limit = 5
-blocks = [block for block in signedblock(genesis_block, limit)]
-assert len(blocks) == limit
-for block in blocks:
-    print("link=" + binascii.hexlify(block[0:64]).decode("iso-8859-1"))
-    print("data=" + block[64:192].decode("iso-8859-1"))
-    print("digest=" + binascii.hexlify(block[192:256]).decode("iso-8859-1"))
-    print("==========================")
-    assert (len(block) == 256)  # 64 (reference) + 128 (data) + 64 (hash)
+blockchain = create_blockchain()
+x = input()
+next(blockchain)
+res = 0
+while x == 'q':
+    res = blockchain.send(res)
+    next(blockchain)
+    print(res)
+    x = input()
